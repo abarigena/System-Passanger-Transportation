834e9a8c6ac98d186ece382e03d62c67bc51bf1a
abarigena <danil.galanov2017@gmail.com>
2025-03-24 00:39:29 +0500

    Добавил сервисы настроил евреку и cloud config на локальную репозиторию

---
+++ raiting-service/src/main/java/com/abarigena/raitingservice/RaitingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.abarigena.raitingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaitingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RaitingServiceApplication.class, args);
+    }
+
+}
---
+++ raiting-service/src/test/java/com/abarigena/raitingservice/RaitingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abarigena.raitingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaitingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
